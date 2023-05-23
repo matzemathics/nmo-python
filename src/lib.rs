b13eedf42a7a325fea13a3f614ef418691cec4a7
Matthias Meissner <geige.matze@gmail.com>
2023-05-23 23:59:38 +0200

    simplify result mapping via trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,26 @@ use std::fs::read_to_string;
 
 use nemo::{
     io::{OutputFileManager, RecordWriter},
-    logical::execution::ExecutionEngine, physical::dictionary::value_serializer::TrieSerializer,
+    logical::execution::ExecutionEngine,
+    physical::dictionary::value_serializer::TrieSerializer,
 };
 use pyo3::{create_exception, prelude::*};
 
 create_exception!(module, NemoError, pyo3::exceptions::PyException);
+
+trait PythonResult {
+    type Value;
+
+    fn py_res(self) -> PyResult<Self::Value>;
+}
+
+impl<T> PythonResult for Result<T, nemo::error::Error> {
+    type Value = T;
+
+    fn py_res(self) -> PyResult<Self::Value> {
+        self.map_err(|err| NemoError::new_err(format!("{}", err)))
+    }
+}
 
 #[pyclass]
 #[derive(Clone)]
@@ -15,15 +30,13 @@ struct NemoProgram(nemo::logical::model::Program);
 #[pyfunction]
 fn load_file(file: String) -> PyResult<NemoProgram> {
     let contents = read_to_string(file)?;
-    let program = nemo::io::parser::parse_program(contents)
-        .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+    let program = nemo::io::parser::parse_program(contents).py_res()?;
     Ok(NemoProgram(program))
 }
 
 #[pyfunction]
 fn load_string(rules: String) -> PyResult<NemoProgram> {
-    let program = nemo::io::parser::parse_program(rules)
-        .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+    let program = nemo::io::parser::parse_program(rules).py_res()?;
     Ok(NemoProgram(program))
 }
 
@@ -35,8 +48,7 @@ impl NemoOutputManager {
     #[new]
     #[pyo3(signature =(path,overwrite=false, gzip=false))]
     fn py_new(path: String, overwrite: bool, gzip: bool) -> PyResult<Self> {
-        let output_manager = OutputFileManager::try_new(path.into(), overwrite, gzip)
-            .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+        let output_manager = OutputFileManager::try_new(path.into(), overwrite, gzip).py_res()?;
 
         Ok(NemoOutputManager(output_manager))
     }
@@ -60,21 +72,13 @@ struct NemoEngine(nemo::logical::execution::DefaultExecutionEngine);
 impl NemoEngine {
     #[new]
     fn py_new(program: NemoProgram) -> PyResult<Self> {
-        let engine = ExecutionEngine::initialize(program.0)
-            .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+        let engine = ExecutionEngine::initialize(program.0).py_res()?;
         Ok(NemoEngine(engine))
     }
 
     fn reason(&mut self) -> PyResult<Vec<NemoIdb>> {
-        self.0
-            .execute()
-            .map_err(|err| NemoError::new_err(format!("{}", err)))?;
-
-        let results = self
-            .0
-            .combine_results()
-            .map_err(|err| NemoError::new_err(format!("{}", err)))?;
-
+        self.0.execute().py_res()?;
+        let results = self.0.combine_results().py_res()?;
         Ok(results.into_iter().map(NemoIdb).collect())
     }
 
@@ -87,12 +91,10 @@ impl NemoEngine {
             .borrow()
             .0
             .create_file_writer(predicate.0.identifier())
-            .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+            .py_res()?;
 
         if let Some(table) = self.0.table_serializer(predicate.0) {
-            writer
-                .write_trie(table)
-                .map_err(|err| NemoError::new_err(format!("{}", err)))?;
+            writer.write_trie(table).py_res()?;
         }
 
         Ok(())
@@ -104,7 +106,12 @@ impl NemoEngine {
         let mut res = Vec::new();
 
         while let Some(record) = table.next_record() {
-            res.push(record.into_iter().map(|v| String::from_utf8(v.as_ref().to_vec()).unwrap()).collect());
+            res.push(
+                record
+                    .into_iter()
+                    .map(|v| String::from_utf8(v.as_ref().to_vec()).unwrap())
+                    .collect(),
+            );
         }
 
         Ok(res)
